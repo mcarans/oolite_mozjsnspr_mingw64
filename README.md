d7a5a071f7528a28c161d54204588128eacf3a2b
Mike <rans@email.com>
2025-09-12 17:14:46 +1200

    Make build scripts

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# mozjsnspr
+To build nspr:
+
+cd nspr
+./build_nspr.sh
+
+To build release JavaScript:
+
+cd js/src
+./build_js_release.sh
+
+To build debug JavaScript:
+
+cd js/src
+./build_js_debug.sh
--- js/src/build_js.sh
+++
@@ -1,5 +0,0 @@
-rm -vrf WINNT5.1_OPT.OBJ
-make BUILD_OPT=1 JS_DIST=../../nspr4/dist JS_THREADSAFE=1 -fMakefile.ref -j16
-make BUILD_OPT=1 JS_DIST=../../nspr4/dist JS_THREADSAFE=1 -fMakefile.ref -j16
-as -o WINNT5.1_OPT.OBJ/methodjit/TrampolinemingwX64.o -mtune=generic64 methodjit/TrampolinemingwX64.s
-./linkit64.bat
